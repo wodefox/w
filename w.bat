786354be76bf2140bf56c0731553fd3407046be1
wodefox <159465206+wodefox@users.noreply.github.com>
2024-04-06 19:31:23 +0800

    Update w.bat
     -ip: 显示所有IP配置信息。
     -a: 显示系统信息。
     -w: 显示所有运行中的进程。
     -b: 显示本地机器上的所有服务。
     -c: 显示计划任务的详细信息。
     -d: 显示工作站统计信息。
     -e: 显示本地机器上的用户账户信息。
     -f: 显示管理员组的成员。
     -g: 显示所有监听端口和相关进程信息。
     -h: 显示此帮助消息。
     -i: 显示本地机器上的所有共享资源。
     -j: 使用wmic显示所有共享资源的名称、路径和状态。
     -k: 使用route print显示路由表。
     -l: 使用arp -a显示所有ARP缓存条目。
     -m: 使用whoami /all显示当前用户的详细信息。
     -n: 显示工作站配置信息。
     -cc: 从源路径复制文件到目标路径。
     -cv: 从源路径移动文件到目标路径。
     -x: 在指定路径创建新文件并写入内容。
     -v: 查看指定目录的所有文件。
     -o: 使用fsutil fsinfo drives显示所有可用驱动器。
     -all: 执行所有命令。

--- w.bat
+++ w.bat
@@ -1,121 +1,141 @@
 @echo off
-SETLOCAL ENABLEDELAYEDEXPANSION
+setlocal
 
-:START
-if "%~1"=="" (
-        echo Usage: w.bat [-ip] [-s "search_string"] [-u URL] [-p URL] [-w] [-kill PID] [-h] [-c URL [SAVE_PATH]] [-cc SOURCE_PATH DEST_PATH] [-cv SOURCE_PATH DEST_PATH] [-v DIR_PATH] [-k FILE_PATH [CONTENT]]
-        echo.
-        echo -ip: Display all IP information.
-        echo -s "search_string": Search for files containing the specified string in their names or contents.
-        echo -u URL: Open the specified URL in the default web browser.
-        echo -p URL: Ping the specified URL.
-        echo -w: List all running processes.
-        echo -kill PID: Kill the process with the specified PID.
-        echo -h: Display this help message.
-        echo -c URL [SAVE_PATH]: Download the file from the specified URL to the given SAVE_PATH.
-        echo -cc SOURCE_PATH DEST_PATH: Copy the file from SOURCE_PATH to DEST_PATH.
-        echo -cv SOURCE_PATH DEST_PATH: Move the file from SOURCE_PATH to DEST_PATH.
-        echo -v DIR_PATH: View the location of the specified directory.
-        echo -k FILE_PATH [CONTENT]: Create a new file with the specified FILE_PATH and write the given CONTENT.
-        exit /b
-) else if "%~1"=="-ip" (
-        echo Displaying all IP information...
-        ipconfig /all
-        echo IP information displayed.
-) else if "%~1"=="-s" (
-        shift
-        if "%~1"=="" (
-             echo Usage: w.bat -s "search_string"
-             exit /b
-        ) else (
-             echo Searching for "%~1" in all files...
-             findstr /s /m /c:"%~1" *.*
-             echo Finished searching.
-        )
-) else if "%~1"=="-u" (
-        if "%~2"=="" (
-             echo Usage: w.bat -u URL
-             exit /b
-        ) else (
-             start "" "%~2"
-        )
-) else if "%~1"=="-p" (
-        if "%~2"=="" (
-             echo Usage: w.bat -p URL
-             exit /b
-        ) else (
-             ping -n 4 "%~2" >nul
-             echo Pinging %~2...
-        )
-) else if "%~1"=="-w" (
-        tasklist
-) else if "%~1"=="-kill" (
-        if "%~2"=="" (
-             echo Usage: w.bat -kill PID
-             exit /b
-        ) else (
-             taskkill /F /PID %~2
-        )
-) else if "%~1"=="-h" (
-        echo Usage: w.bat [-ip] [-s "search_string"] [-u URL] [-p URL] [-w] [-kill PID] [-h] [-c URL [SAVE_PATH]] [-cc SOURCE_PATH DEST_PATH] [-cv SOURCE_PATH DEST_PATH] [-v DIR_PATH] [-k FILE_PATH [CONTENT]]
-        echo.
-        echo -ip: Display all IP information.
-        echo -s "search_string": Search for files containing the specified string in their names or contents.
-        echo -u URL: Open the specified URL in the default web browser.
-        echo -p URL: Ping the specified URL.
-        echo -w: List all running processes.
-        echo -kill PID: Kill the process with the specified PID.
-        echo -h: Display this help message.
-        echo -c URL [SAVE_PATH]: Download the file from the specified URL to the given SAVE_PATH.
-        echo -cc SOURCE_PATH DEST_PATH: Copy the file from SOURCE_PATH to DEST_PATH.
-        echo -cv SOURCE_PATH DEST_PATH: Move the file from SOURCE_PATH to DEST_PATH.
-        echo -v DIR_PATH: View the location of the specified directory.
-        echo -k FILE_PATH [CONTENT]: Create a new file with the specified FILE_PATH and write the given CONTENT.
-        exit /b
-) else if "%~1"=="-c" (
-        if "%~2"=="" (
-             echo Usage: w.bat -c URL [SAVE_PATH]
-             exit /b
-        ) else (
-             curl -o "%~2" "%~1"
-             echo Downloading %~1 to %~2...
-        )
-) else if "%~1"=="-cc" (
-        if "%~2"=="" (
-             echo Usage: w.bat -cc SOURCE_PATH DEST_PATH
-             exit /b
-        ) else (
-             copy "%~1" "%~2"
-             echo Copied %~1 to %~2
-        )
-) else if "%~1"=="-cv" (
-        if "%~2"=="" (
-             echo Usage: w.bat -cv SOURCE_PATH DEST_PATH
-             exit /b
-        ) else (
-             move "%~1" "%~2"
-             echo Moved %~1 to %~2
-        )
-) else if "%~1"=="-v" (
-        if "%~2"=="" (
-             echo Usage: w.bat -v DIR_PATH
-             exit /b
-        ) else (
-             echo The location of %~2 is:
-             cd "%~2" && echo %CD%
-        )
-) else if "%~1"=="-k" (
-        if "%~2"=="" (
-             echo Usage: w.bat -k FILE_PATH [CONTENT]
-             exit /b
-        ) else (
-             echo Creating file %~2...
-             echo %~3 > "%~2"
-             echo File %~2 has been created with content: %~3
-        )
-) else (
-        echo Unknown option: %~1
-        exit /b
+:parse_args
+if "%1"=="" (
+    echo Usage: w.bat [-ip] [-a] [-w] [-b] [-c] [-d] [-e] [-f] [-g] [-h] [-i] [-j] [-k] [-l] [-m] [-n] [-cc] [-cv] [-x] [-v] [-o] [-all]
+    echo -ip: Display all IP configuration information.
+    echo -a: Display system information.
+    echo -w: Display all running processes.
+    echo -b: Display all services on the local machine.
+    echo -c: Display detailed information about scheduled tasks.
+    echo -d: Display all workstation statistics.
+    echo -e: Display user accounts on the local machine.
+    echo -f: Display members of the Administrators local group.
+    echo -g: Display all listening ports and associated process information.
+    echo -h: Display this help message.
+    echo -i: Display all shared resources on the local machine.
+    echo -j: Display all shared resources with name, path, and status using wmic.
+    echo -k: Display the routing table using route print.
+    echo -l: Display all ARP cache entries using arp -a.
+    echo -m: Display detailed information about the current user using whoami /all.
+    echo -n: Display workstation configuration information using net config workstation.
+    echo -cc: Copy a file from source path to destination path.
+    echo -cv: Move a file from source path to destination path.
+    echo -x: Create a new file at the specified path and write content to it.
+    echo -v: View all files in the specified directory.
+    echo -o: Display all available drives using fsutil fsinfo drives.
+    echo -all: Execute all commands.
+    goto :eof
 )
 
-ENDLOCAL
+if "%1"=="-ip" (
+    ipconfig /all
+) else if "%1"=="-a" (
+    systeminfo
+) else if "%1"=="-w" (
+    tasklist
+) else if "%1"=="-b" (
+    sc query
+) else if "%1"=="-c" (
+    schtasks /query /fo list /v
+) else if "%1"=="-d" (
+    net statistics workstation
+) else if "%1"=="-e" (
+    net user
+) else if "%1"=="-f" (
+    net localgroup administrators
+) else if "%1"=="-g" (
+    netstat -ano
+) else if "%1"=="-h" (
+    echo Usage: w.bat [-ip] [-a] [-w] [-b] [-c] [-d] [-e] [-f] [-g] [-h] [-i] [-j] [-k] [-l] [-m] [-n] [-cc] [-cv] [-x] [-v] [-o] [-all]
+) else if "%1"=="-i" (
+    net share
+) else if "%1"=="-j" (
+    wmic share get name,path,status
+) else if "%1"=="-k" (
+    route print
+) else if "%1"=="-l" (
+    arp -a
+) else if "%1"=="-m" (
+    whoami /all
+) else if "%1"=="-n" (
+    net config workstation
+) else if "%1"=="-cc" (
+    if "%2"=="" (
+         echo Error: No source or destination path specified for -cc.
+         goto :eof
+        ) else if "%3"=="" (
+         echo Error: No destination path specified for -cc.
+         goto :eof
+        ) else (
+         copy "%2" "%3"
+         if %errorlevel% equ 0 (
+             echo File copied successfully from %2 to %3.
+            ) else (
+             echo Failed to copy the file.
+            )
+        )
+) else if "%1"=="-cv" (
+    if "%2"=="" (
+         echo Error: No source or destination path specified for -cv.
+         goto :eof
+        ) else if "%3"=="" (
+         echo Error: No destination path specified for -cv.
+         goto :eof
+        ) else (
+         move "%2" "%3"
+         if %errorlevel% equ 0 (
+             echo File moved successfully from %2 to %3.
+            ) else (
+             echo Failed to move the file.
+            )
+        )
+) else if "%1"=="-x" (
+    if "%2"=="" (
+         echo Error: No file path specified for -x.
+         goto :eof
+        ) else (
+         echo %3 > "%2"
+         if %errorlevel% equ 0 (
+             echo File created and content written successfully to %2.
+            ) else (
+             echo Failed to create the file or write content.
+            )
+        )
+) else if "%1"=="-v" (
+    if "%2"=="" (
+         echo Error: No directory path specified for -v.
+         goto :eof
+        ) else (
+         dir "%2"
+         if %errorlevel% equ 0 (
+             echo Directory contents listed successfully for %2.
+            ) else (
+             echo Failed to list directory contents.
+            )
+        )
+) else if "%1"=="-o" (
+    fsutil fsinfo drives
+) else if "%1"=="-all" (
+    ipconfig /all
+    systeminfo
+    tasklist
+    sc query
+    schtasks /query /fo list /v
+    net statistics workstation
+    net user
+    net localgroup administrators
+    netstat -ano
+    wmic share get name,path,status
+    route print
+    arp -a
+    whoami /all
+    net config workstation
+    fsutil fsinfo drives
+) else (
+    echo Unknown argument: %1
+)
+
+shift
+goto :parse_args
